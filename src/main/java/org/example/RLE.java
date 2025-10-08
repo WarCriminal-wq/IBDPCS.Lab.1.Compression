b71fb410135be0357ef462cf97bd73b3285b0b6c
Th3FooI <Icebearman@outlook.com>
2025-10-08 09:40:46 +0800

    diddy

--- src/main/java/org/example/RLE.java
+++ src/main/java/org/example/RLE.java
@@ -12,7 +12,9 @@ public class RLE {
         System.out.println(compressedString);
     }
 
-    /** This method converts the information stored in a text file into a String. */
+    /**
+     * This method converts the information stored in a text file into a String.
+     */
     public static String textToString(String filename) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(filename));
         StringBuilder sb = new StringBuilder();
@@ -25,13 +27,60 @@ public class RLE {
         return sb.toString();
     }
 
-    /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
-    public static String compress(String uncompressed) { return null; }
+    /**
+     * TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String.
+     */
+    public static String compress(String uncompressed) {
+        if (uncompressed.isEmpty() || uncompressed == null) {
+            return null;
+        }
 
-    /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
-    public static String decompress(String compressed) {
-        return null;
+        StringBuilder str = new StringBuilder();
+        int counter = 1;
+
+        for (int i = 1; i <= uncompressed.length(); i++) {
+
+            if (i == uncompressed.length() || uncompressed.charAt(i) != uncompressed.charAt(i - 1)) {
+                str.append(counter);
+                str.append(uncompressed.charAt(i - 1));
+                counter = 1;
+            } else {
+                counter++;
+            }
+        }
+
+        return str.toString();
     }
 
+    /**
+     * TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String.
+     */
+    public static String decompress(String compressed) {
+        if (compressed.isEmpty() || compressed == null) {
+            return null;
+        }
+        StringBuilder str = new StringBuilder();
 
+        int i = 0;
+
+        while (i < compressed.length()) {
+            StringBuilder countBuilder = new StringBuilder();
+            while (i < compressed.length() && Character.isDigit(compressed.charAt(i))) {
+                countBuilder.append(compressed.charAt(i));
+                i++;
+            }
+
+            if (i < compressed.length()) {
+                char character = compressed.charAt(i);
+                i++;
+                int count = Integer.parseInt(countBuilder.toString());
+                for (int j = 0; j < count; j++) {
+                    str.append(character);
+                }
+            }
+        }
+
+        return str.toString();
+
+    }
 }
